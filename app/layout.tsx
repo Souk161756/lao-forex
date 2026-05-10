a021937b23ade12c631a2df10f081ab9db1b070a
Souk <youremail@gmail.com>
2026-05-10 14:39:58 +0700

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KE4H9CXRXN"></script>
+
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-KE4H9CXRXN');
+    `,
+  }}
+/>
+        </body>
     </html>
   );
 }
