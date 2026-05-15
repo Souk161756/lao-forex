173f339f0bcf12e29715b5e138109fad00b01bfe
Souk <youremail@gmail.com>
2026-05-15 15:03:35 +0700

    analytics fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "LaoForex.org - Best Forex Brokers Laos",
-  description:
-    "รีวิว Forex Brokers ในลาว เปรียบเทียบ XM Exness Vantage IUX HFM และ cashback Forex ล่าสุด",
+  title: "LaoForex.org",
+  description: "Best Forex Brokers In Laos",
 };
 
 export default function RootLayout({
@@ -15,38 +14,24 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KE4H9CXRXN"></script>
 
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KE4H9XCRXN"
+        />
 
-      gtag('config', 'G-KE4H9CXRXN');
-    `,
-  }}
-/><script
-  async
-  src="https://www.googletagmanager.com/gtag/js?id=G-KE4H9XCRXN"
-/>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-KE4H9XCRXN');
-    `,
-  }}
-/>
-        </body>
-   
-    `,
-
+              gtag('config', 'G-KE4H9XCRXN');
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
