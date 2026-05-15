950ca6c9424edf6f4b0349304f34a2da96a73e0e
Souk <youremail@gmail.com>
2026-05-15 14:53:06 +0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,22 @@ export default function RootLayout({
   }}
 />
         </body>
+        <script
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-KE4H9XCRXN\"
+/>
+
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-KE4H9XCRXN');
+    `,
+  }}
+/>
     </html>
   );
 }
